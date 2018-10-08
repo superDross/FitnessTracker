e88a70624063b9ea97746dfdbe3f22b3d4a6d579
david <dross78375@gmail.com>
2018-10-08 19:16:27 +0100

    Introduced more models

--- fitness/tracker/models.py
+++ fitness/tracker/models.py
@@ -1,4 +1,7 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class MuscleGroup(models.Model):
@@ -10,15 +13,15 @@ class MuscleGroup(models.Model):
         ('Legs', 'Legs'),
     )
 
-    muscle_group = models.CharField(max_length=20,
-                                    choices=MUSCLE_GROUPS,
-                                    help_text='name of body area')
+    group = models.CharField(max_length=20,
+                             choices=MUSCLE_GROUPS,
+                             help_text='name of body area')
 
     muscle = models.CharField(max_length=50,
                               help_text='name of body area')
 
     def __str__(self):
-        return '{}: {}'.format(self.muscle_group, self.muscle)
+        return '{}: {}'.format(self.group, self.muscle)
 
 
 class Classification(models.Model):
@@ -32,23 +35,74 @@ class Classification(models.Model):
 
 class Exercise(models.Model):
     ''' A sepcific exercise e.g. pushups'''
+    EXERCISE_TYPE = (
+        ('m', 'Isometric'),
+        ('i', 'Isotonic'),
+        ('c', 'Cardiovascular'),
+    )
+
     name = models.CharField(max_length=50,
                             help_text='Name of movement')
     description = models.CharField(max_length=1000,
                                    help_text='Movement description.')
+    type = models.CharField(
+        max_length=20,
+        choices=EXERCISE_TYPE,
+        blank=True,
+        help_text='Exercise catagory'
+    )
     video = models.URLField(max_length=500,
                             help_text='Video demonstrating exercise.')
+
     classification = models.ForeignKey(Classification,
                                        on_delete=models.SET_NULL,
                                        null=True)
     muscles = models.ManyToManyField(MuscleGroup)
+    progression = models.OneToOneField('self',
+                                       on_delete=models.SET_NULL,
+                                       null=True)
+    creator = models.ForeignKey(User,
+                                models.SET_NULL,
+                                null=True,
+                                blank=True)
 
     def __str__(self):
         return self.name
 
 
+class Reps(models.Model):
+    ''' Number of repetions of a movement.'''
+    number = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'Reps - {self.number}'
+
+
+class Sets(models.Model):
+    ''' A set of repetitions of movements.'''
+    number = models.IntegerField(default=1)
+    reps = models.ManyToManyField(Reps)
+    weight = models.IntegerField(default=0)
+    time = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Sets - {self.number}'
+
+
 class ExerciseInstance(models.Model):
     '''A specific execution of the exercise'''
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          editable=False, unique=True)
     exercise = models.ForeignKey(Exercise, on_delete=models.SET_NULL,
                                  null=True)
     date = models.DateField(null=True, blank=True)
+    set = models.ForeignKey(Sets,
+                            on_delete=models.SET_NULL,
+                            null=True)
+    participant = models.ForeignKey(User,
+                                    models.SET_NULL,
+                                    null=True,
+                                    blank=True)
+
+    def __str__(self):
+        return f'{self.id} ({self.exercise.name})'
