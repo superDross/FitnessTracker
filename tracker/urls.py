ea12a77c7652634ffc0bf98e9a1e8e5e9c10f5eb
david <dross78375@gmail.com>
2018-10-28 21:53:48 +0000

    Added an exercise list view

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from tracker import views
 
 urlpatterns = [
-
+    path('exercise-list/', views.ExerciseView.as_view(), name='exercises'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def index(request):
-    test
+from .models import Exercise
 
-# Create your views here.
+
+class ExerciseView(LoginRequiredMixin, generic.ListView):
+    model = Exercise
+    template_name = 'tracker/exercise_list.html'
+    context_object_name = 'exercise_list'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(profile__username=self.kwargs['username'])
