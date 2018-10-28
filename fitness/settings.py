b0dfe466a84235e2cd2127d8ab7e37328bad37de
david <dross78375@gmail.com>
2018-10-28 21:54:29 +0000

    Added login/logout pages

--- fitness/settings.py
+++ fitness/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'fitness.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/tracker'
