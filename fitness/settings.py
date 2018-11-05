26a47cd99f3bbfd6ac005eafac821b87dad6180a
david <dross78375@gmail.com>
2018-11-05 22:52:56 +0000

    getattr() function now available as a tag.

--- fitness/settings.py
+++ fitness/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker.apps.TrackerConfig',
+    'templatetags',
     'django_extensions',
     'django_countries',
 ]
