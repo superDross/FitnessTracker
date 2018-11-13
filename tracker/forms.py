ca5aaea8a01d7e04a37611fd873bdcb79364a7bc
david <dross78375@gmail.com>
2018-11-13 18:27:58 +0000

    Sorts by date before returning table

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from django.forms.widgets import SelectDateWidget
 
+from .models import Exercise
+
 
 class DateForm(forms.Form):
     start_date = forms.DateField(widget=SelectDateWidget())
     end_date = forms.DateField(widget=SelectDateWidget())
+
+class ExerciseForm(forms.Form):
+    exercise = forms.ModelChoiceField(queryset=Exercise.objects.all())
